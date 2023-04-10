2492ca88994bdaceac4b9b7eb69229cada61e317
turulomio <turulomio@yahoo.es>
2023-04-10 10:08:06 +0200

    Added tests

--- pydicts/tests/test_year_month_value.py
+++ pydicts/tests/test_year_month_value.py
@@ -1,13 +1,12 @@
 from pydicts.year_month_value import lod_year_month_value_transposition
 
-def tests_dictkv():
-  
-    
-    print ("-- List dict transposition")
+def tests_lod_year_month_value_transposition():
     o=[
         {"year": 2022, "month": 1, "my_sum": 12},
         {"year": 2021, "month": 2, "my_sum": 123},
         {"year": 2019, "month": 5, "my_sum": 1},
         {"year": 2022, "month": 12, "my_sum": 12},
     ]
-    print(lod_year_month_value_transposition(o,key_value="my_sum"))
+    t=lod_year_month_value_transposition(o,key_value="my_sum")
+    assert t[0]["year"]==2019
+    assert t[3]["total"]==24
