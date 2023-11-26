3ee7f950bbc4ce0a78f75f6e1f19cfaf5cb669f4
turulomio <turulomio@yahoo.es>
2023-11-26 09:10:35 +0100

    Migrating casts and datetime_functions to pydicts.casts

---
+++ pydicts/lol.py
@@ -0,0 +1,75 @@
+from logging import warning
+
+## LOR is a list of list. Naned List Of Rows, used in myqtablewidget for example
+## @param rows LOR
+## @param index int with the index of the position where we are going to insert row
+## @param column List with the values to add. Must be of the same size of rows
+def lol_add_column(rows, index, column):
+    if len(rows)!=len(column):
+        raise Exception("I can't add a column with different size of lol")
+    r_rows=[]
+    for i, row in enumerate(rows):
+        r_rows.append(row[0:index] + [column[i],] + row[index:len(row)])
+    return r_rows
+
+## lol is a list of list. Naned List Of Rows, used in myqtablewidget
+## @param listindex is a list of column indexes to remove
+def lol_remove_columns(rows, listindex):
+    r_rows=[]
+    for i, row in enumerate(rows):
+        r_rows.append(list_remove_positions(row,listindex))
+    return r_rows
+
+## lol is a list of list. Naned List Of Rows, used in myqtablewidget
+## @param listindex is a list of column indexes to remove
+def lol_remove_rows(rows, listindex):
+    return list_remove_positions(rows, listindex) #It's a list but of row
+
+## Return a lol transposed. Changed rows by columns
+def lol_transposed(lol):
+    if len(lol)==0:
+        return []
+    r=[]
+    columns=len(lol[0])
+    for column in range(columns):
+        tran_row=[]
+        for row in lol:
+            tran_row.append(row[column])
+        r.append(tran_row)
+    return r
+
+## Extract a column from the list of row
+def lol_get_row(lol, row):
+    return lol[row]
+
+## Extract a column from the list of row
+def lol_get_column(lol, column):
+    r=[]
+    for row in lol:
+        r.append(row[column])
+    return r
+
+## Return sum of values of a column from and index to and other index position. This method ignores None values
+## This method can sum several objects
+## @param row
+## @param from_index
+## @param to_index
+## @param zerovalue 0 or Money(self.mem, 0, self.mem.localcurrency)....
+def lol_sum_row(row, from_index, to_index, zerovalue=0):
+    s=zerovalue
+    for i, column in enumerate(row):
+        if i>=from_index and i<=to_index:
+            if column is not None:
+                s=s + column
+    return s
+
+## Return sum of values of a column from and index to and other index position. This method ignores None values
+## This method can sum several objects
+## @zerovalue 0 or Money(self.mem, 0, self.mem.localcurrency)....
+def lol_sum_column(lol, column, from_index, to_index, zerovalue=0):
+    s=zerovalue
+    for i, row in enumerate(lol):
+        if i>=from_index and i<=to_index:
+            if row[column] is not None:
+                s=s + row[column]
+    return s
