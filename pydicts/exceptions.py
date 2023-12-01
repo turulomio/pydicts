f579360cecfdf629991e6788f6941d73da9cb11d
turulomio <turulomio@yahoo.es>
2023-12-01 05:22:56 +0100

    Created custom exceptions

---
+++ pydicts/exceptions.py
@@ -0,0 +1,14 @@
+class LodException(Exception):
+    pass
+    
+class CastException(Exception):
+    pass
+    
+class LodXYVException(Exception):
+    pass
+    
+class LodYMVException(Exception):
+    pass
+    
+class LolException(Exception):
+    pass
