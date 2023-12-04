c252a982ad41c459b0a60e82c6cd6710b5e546fe
turulomio <turulomio@yahoo.es>
2023-12-04 20:05:19 +0100

    pydicts-0.9.0

--- pydicts/__init__.py
+++ pydicts/__init__.py
@@ -1,4 +1,4 @@
 from datetime import datetime
-__version__="0.8.0"
-__versiondatetime__= datetime(2023, 11, 26, 17, 53)
+__version__="0.9.0"
+__versiondatetime__= datetime(2023, 12, 4, 20, 0)
 __versiondate__=__versiondatetime__.date()
