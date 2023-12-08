550b4ff191e30975963670c34769f593d2f740bc
turulomio <turulomio@yahoo.es>
2023-12-08 09:42:30 +0100

    pydicts-0.10.0 (#30)
    * Moved currency and percentage modules  from unogenerator
    
    * Added basic tests
    
    * Added basic tests
    
    * pydicts-0.10.0

---
+++ pydicts/tests/test_currency.py
@@ -0,0 +1,3 @@
+from pydicts.currency import Currency
+def test_currency():
+    assert Currency(None).amount== 0
---
+++ pydicts/tests/test_percentage.py
@@ -0,0 +1,3 @@
+from pydicts.percentage import Percentage
+def test_percentage():
+    assert Percentage(None).value== None
