484a9febe5cb5956b4b7f1ea25aa19eaac6defc0
turulomio <turulomio@yahoo.es>
2024-03-21 03:45:17 +0100

    Added lod_remove_duplicates. Refactorized lod to lod_.

--- pydicts/lod.py
+++ pydicts/lod.py
@@ -9,10 +9,10 @@ try:
 except:
     _=str
 
-def lod_has_key(lod, key):
-    if len(lod)==0:
+def lod_has_key(lod_, key):
+    if len(lod_)==0:
         return False
-    return key in lod[0]
+    return key in lod_[0]
 
 ## Order data columns. None values are set at the beginning
 def lod_order_by(ld, key, reverse=False, none_at_top=True):
@@ -30,82 +30,82 @@ def lod_order_by(ld, key, reverse=False, none_at_top=True):
     else:
         return nonull+null
 
-def lod_print(lod, number=None):
+def lod_print(lod_, number=None):
     """
     Function Prints a list of dictionaries with tabulate module.
 
-    @param lod
+    @param lod_
     @type List of dictionaries
-    @param number Number of dictionaries in the list to print. If None prints all lod. (defaults to None)
+    @param number Number of dictionaries in the list to print. If None prints all lod_. (defaults to None)
     @type Integer
     """
-    number=len(lod) if number is None else number
-    if len(lod)==0:
+    number=len(lod_) if number is None else number
+    if len(lod_)==0:
         print(_("This list of dictionaries hasn't data to print"))
-    print(tabulate(lod[0:number], headers="keys", tablefmt="psql"))
+    print(tabulate(lod_[0:number], headers="keys", tablefmt="psql"))
 
-def lod_sum(lod, key, ignore_nones=True):
+def lod_sum(lod_, key, ignore_nones=True):
     r=0
-    for d in lod:
+    for d in lod_:
         if ignore_nones is True and d[key] is None:
             continue
         r=r+d[key]
     return r
 
-def lod_sum_negatives(lod, key):
+def lod_sum_negatives(lod_, key):
     r=0
-    for d in lod:
+    for d in lod_:
         if d[key] is None or d[key]>0:
             continue
         r=r+d[key]
     return r
 
-def lod_sum_positives(lod, key):
+def lod_sum_positives(lod_, key):
     r=0
-    for d in lod:
+    for d in lod_:
         if d[key] is None or d[key]<0:
             continue
         r=r+d[key]
     return r
 
-def lod_average(lod, key):
-    return lod_sum(lod,key)/len(lod)
+def lod_average(lod_, key):
+    return lod_sum(lod_,key)/len(lod_)
 
-def lod_average_ponderated(lod, key_numbers, key_values):
+def lod_average_ponderated(lod_, key_numbers, key_values):
     prods=0
-    for d in lod:
+    for d in lod_:
         prods=prods+d[key_numbers]*d[key_values]
-    return prods/lod_sum(lod, key_numbers)
+    return prods/lod_sum(lod_, key_numbers)
 
-def lod_median(lod, key):
+def lod_median(lod_, key):
     from statistics import median
-    return median(lod2list(lod, key, sorted=True))
+    return median(lod2list(lod_, key, sorted=True))
 
-## Converts a lod to a dict using key as new dict key, and value as the key of the value field
-def lod2dictkv(lod, key, value):
+## Converts a lod_ to a dict using key as new dict key, and value as the key of the value field
+def lod2dictkv(lod_, key, value):
     d={}
-    for ld in lod:
+    for ld in lod_:
         d[ld[key]]=ld[value]
     return d
 
-## Converts a lod to a dict using key as new dict key, and the dict as a value
-def lod2dod(lod, key):
+## Converts a lod_ to a dict using key as new dict key, and the dict as a value
+def lod2dod(lod_, key):
     d={}
-    for ld in lod:
+    for ld in lod_:
         d[ld[key]]=ld
     return d
     
-## Converts a lod to an ordered dictionary of dictionarys
-def lod2odod(lod, key):
+## Converts a lod_ to an ordered dictionary of dictionarys
+def lod2odod(lod_, key):
     d=OrderedDict()
-    for ld in lod:
+    for ld in lod_:
         d[ld[key]]=ld
     return d
     
-## Converts a lod to a dict using (key1,key2) tuple  as new dict key, and the dict as a value
-def lod2dod_tuple(lod, key1, key2):
+## Converts a lod_ to a dict using (key1,key2) tuple  as new dict key, and the dict as a value
+def lod2dod_tuple(lod_, key1, key2):
     d={}
-    for ld in lod:
+    for ld in lod_:
         d[(ld[key1],ld[key2])]=ld
     return d
 
@@ -116,14 +116,14 @@ def dod2lod(d):
         r.append(v)
     return r
 
-## Returns a list from a lod key
-## @param lod
+## Returns a list from a lod_ key
+## @param lod_
 ## @param key String with the key to extract
 ## @param sorted Boolean. If true sorts final result
 ## @param cast String. "str", "float", casts the content of the key
-def lod2list(lod, key, sorted=False, cast=None):
+def lod2list(lod_, key, sorted=False, cast=None):
     r=[]
-    for ld in lod:
+    for ld in lod_:
         if cast is None:
             r.append(ld[key])
         elif cast == "str":
@@ -134,14 +134,14 @@ def lod2list(lod, key, sorted=False, cast=None):
         r.sort()
     return r
 
-## Returns a list from a lod key, with distinct values, not all values
-## @param lod
+## Returns a list from a lod_ key, with distinct values, not all values
+## @param lod_
 ## @param key String with the key to extract
 ## @param sorted Boolean. If true sorts final result
 ## @param cast String. "str", "float", casts the content of the key
-def lod2list_distinct(lod, key, sorted=False, cast=None):
+def lod2list_distinct(lod_, key, sorted=False, cast=None):
     set_=set()
-    for ld in lod:
+    for ld in lod_:
         if cast is None:
             set_.add(ld[key])
         elif cast == "str":
@@ -156,15 +156,15 @@ def lod2list_distinct(lod, key, sorted=False, cast=None):
 
 ## Converts a list of ordereddict to a list of rows. ONLY DATA
 ## @params keys If None we must suppose is an ordered dict or keys will be randomized
-def lod2lol(lod,  keys=None):
-    if len(lod)==0:
+def lod2lol(lod_,  keys=None):
+    if len(lod_)==0:
         return []
         
     if keys is None:
-        keys=lod[0].keys()
+        keys=lod_[0].keys()
         
     r=[]  
-    for od in lod:
+    for od in lod_:
         row_r=[]
         for key in keys:
             row_r.append(od[key])
@@ -184,7 +184,7 @@ def lod2lood(ld, keys):
     return r
 
 
-## Returns maximum value of a given key. Is unique. REturns NOne if lod is empty
+## Returns maximum value of a given key. Is unique. REturns NOne if lod_ is empty
 def lod_max_value(ld, key):
      if len(ld)>0:
           r=ld[0][key]
@@ -195,7 +195,7 @@ def lod_max_value(ld, key):
              r=d[key]
      return r
 
-## Returns minimum value of a given key. Is unique. REturns NOne if lod is empty
+## Returns minimum value of a given key. Is unique. REturns NOne if lod_ is empty
 def lod_min_value(ld, key):
      if len(ld)>0:
           r=ld[0][key]
@@ -206,6 +206,24 @@ def lod_min_value(ld, key):
              r=d[key]
      return r
 
+def lod_remove_duplicates(lod_):
+    """
+        Remove duplicated dictionaries (same keys and values) from a list of dictionaries
+        Params:
+            - lod_ List of dictionaries
+        Returns:
+            A list of dictionaries
+    """
+    seen = set()
+    unique_dicts = []
+    for d in lod_:
+        # Convert dictionary to a tuple of its items for hashability
+        t = tuple(d.items())
+        if t not in seen:
+            seen.add(t)
+            unique_dicts.append(d)
+    return unique_dicts
+
 def lod_rename_key(ld, from_, to_):
     """
         Renames a key(from_) to another key(to_) in each dictionary in ld
@@ -214,18 +232,18 @@ def lod_rename_key(ld, from_, to_):
         d[to_]=d.pop(from_)
     return ld
 
-def lod_remove_key(lod, key):
+def lod_remove_key(lod_, key):
     """
         Removes a key from all dictionaries in a list of dictionaries
     """
-    for d in lod:
+    for d in lod_:
         del d[key]
-    return lod
+    return lod_
     
 def lod_keys(lod_):
     """
         Return the keys of the list of dicts
-        Return None if length of lod is 0. 
+        Return None if length of lod_ is 0. 
         Retuurn the keys of the first dictionary as a list
     """
     if len(lod_)>0:
@@ -243,8 +261,8 @@ def lod_clone(lod_):
 
 def lod_filter_keys(lod_, keys):
     """
-        Create a new lod leaving only keys in parameter
-        @param keys List of keys to copy in new lod
+        Create a new lod_ leaving only keys in parameter
+        @param keys List of keys to copy in new lod_
     """
     new_lod=[]
     for d in lod_:
@@ -257,7 +275,7 @@ def lod_filter_keys(lod_, keys):
 
 def lod_filter_dictionaries(lod_, lambda_function):
     """
-        Create a new lod leaving filtering dictionaries that returns True to lambda funcion
+        Create a new lod_ leaving filtering dictionaries that returns True to lambda funcion
         @param lambda_function needs two parameters (d, index) where d is the dictionary used to iterate and index is the position of the dictionary in the list of dictionaries
     """
     new_lod=[]
@@ -268,7 +286,7 @@ def lod_filter_dictionaries(lod_, lambda_function):
     
 def lod_calculate(lod_, key, lambda_function, clone=False):
     """
-        Creates or replaces a column with the result of tthe lambda function in the lod passed as parameter
+        Creates or replaces a column with the result of tthe lambda function in the lod_ passed as parameter
         @param key  Dictionary key where lambda_function result is going to be set
         @param lambda_function needs two parameters (d, index) where d is the dictionary used to iterate and index is the position of the dictionary in the list of dictionaries
         @param clone if True returns a clone of the lod_ parameter
