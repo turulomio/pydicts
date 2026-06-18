365e24d17419a8be543502ffde599f3fd0b03df2
turulomio <turulomio@yahoo.es>
2026-06-18 08:31:43 +0200

    pydicts-1.5.0 (#128)

--- pydicts/__init__.py
+++ pydicts/__init__.py
@@ -2,6 +2,6 @@
 PyDicts: A Python library for convenient dictionary and list of dictionary manipulations.
 """
 from datetime import datetime
-__version__="1.4.0"
-__versiondatetime__= datetime(2026, 5, 25, 7, 48)
+__version__="1.5.0"
+__versiondatetime__= datetime(2026, 6, 18, 8, 27)
 __versiondate__=__versiondatetime__.date()
